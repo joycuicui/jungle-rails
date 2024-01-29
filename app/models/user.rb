cc7bc8c3ffb1ee5ecc410c078fede9f0a1b0698b
joycuicui <cuiqijoy@outlook.com>
2024-01-29 15:22:25 -0800

    add rspec for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :email, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,92 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    let(:user_attributes) do
+      {
+        first_name: "Jane",
+        last_name: "Doe", 
+        email: "jane@email.com",
+        password: "123456",
+        password_confirmation: "123456"
+      }
+    end
+
+    let(:user) { User.new(user_attributes) }
+
+    it "is valid with valid attributes" do
+      expect(user).to be_valid
+    end
+
+    it "requires a first name" do
+      user.first_name = nil
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    it "requires a last name" do
+      user.last_name = nil
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to include("Last name can't be blank")
+    end
+
+    it "requires an email" do
+      user.email = nil
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    context "password validations" do
+    
+      it "requires a password" do
+        user.password = nil
+        expect(user).to_not be_valid
+        expect(user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it "is not valid when password and password_confirmation do not match" do
+        user.password_confirmation = "654321"
+        expect(user).to_not be_valid
+        expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it "is not valid when password length is less than 6" do
+        user.password = "123"
+        expect(user).to_not be_valid
+        expect(user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+    
+    end
+
+    context "email should be unique" do
+
+      it "is not valid when the email already exist" do
+        user.save()
+        another_user = User.new(
+          first_name: "John",
+          last_name: "Doe", 
+          email: "jane@email.com",
+          password: "123456",
+          password_confirmation: "123456"
+        )
+        expect(another_user).to_not be_valid
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+
+      it "is not case sensitive" do
+        user.save()
+        another_user = User.new(
+          first_name: "John",
+          last_name: "Doe", 
+          email: "JANE@EMAIL.COM",
+          password: "123456",
+          password_confirmation: "123456"
+        )
+        expect(another_user).to_not be_valid
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+    
+    end
+
+  end
 end
